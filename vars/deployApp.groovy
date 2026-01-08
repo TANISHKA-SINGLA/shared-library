7a1fe5de9c05718b4a3c24ff0fbb4bac2333ab82
Tanishka Singla <tannusingla185@gmail.com>
2026-01-08 15:43:17 +0530

    lib

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,18 +1,3 @@
 def call(String env) {
-
-    if (!env) {
-        env = 'dev'
-        echo "No env provided, defaulting to DEV"
-    }
-
-    if (env == 'dev') {
-        echo "Deploying to DEV environment"
-    } else if (env == 'qa') {
-        echo "Deploying to QA environment"
-    } else if (env == 'prod') {
-        echo "Deploying to PROD environment"
-    } else {
-        error "Invalid environment selected: ${env}"
-    }
+    echo "Deploying application to ${env}"
 }
-
